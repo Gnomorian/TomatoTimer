9eee52ea1f6c02a0bff9ac8c8df87f5cabf50b76
William Cameron <william-cameron1994@live.co.uk>
2020-07-23 04:07:45 +1200

    added some logging

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 #include "timerform.h"
 
 #include <QApplication>
+#include <fstream>
+
+const QString messagePattern = "%{time} [%{type}%{if-debug} function=%{function}, line=%{line}, file=%{file}%{endif}] %{message}";
+
+void messageHandler(QtMsgType type, const QMessageLogContext& context, const QString& message)
+{
+    static std::fstream logfile("TomatoTimer.log", std::ios_base::app);
+    logfile << qPrintable(qFormatLogMessage(type, context, message)) << std::endl;
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    (void)qInstallMessageHandler(messageHandler);
+    qSetMessagePattern(messagePattern);
+
     TimerForm w;
     w.show();
-    return a.exec();
+    const auto result = a.exec();
+    qInfo("TomatoTimer exited with %i", result);
+    return result;
 }
